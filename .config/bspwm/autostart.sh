e1572bf122645157e15ebbb9233097e193d8ceac
Richard Bendli <93144317+richardbendli@users.noreply.github.com>
2023-06-03 12:56:17 +0100

    conf

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -15,6 +15,9 @@ picom --config $HOME/.config/bspwm/picom.conf &
 #conky -c $HOME/.config/bspwm/system-overview &
 xsetroot -cursor_name left_ptr &
 
+# rofi wifi menu
+#$HOME/.config/rofi/wifimenu/rofi-wifi-menu.sh &
+
 #Find out your monitor name with xrandr or arandr (save and you get this line)
 #xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal
 #xrandr --output DP2 --primary --mode 1920x1080 --rate 60.00 --output LVDS1 --off &
